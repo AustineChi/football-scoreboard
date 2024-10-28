5728d52525d11dcd99b5cdab270a222cc13f87ae
AustineChi <emeghara.augustine@gmail.com>
2024-10-28 16:35:55 +0000

    Implement Match model

---
+++ football-scoreboard-library/src/core/match/Match.model.ts
@@ -0,0 +1,10 @@
+export interface Match {
+    id: string;
+    homeTeam: string;
+    awayTeam: string;
+    homeScore: number;
+    awayScore: number;
+    startTime: Date;
+    isFinished: boolean;
+  }
+  
